d0da43d70a11ef0059f5eb77a2b7ecc5a33c4b99
Yevgeniy Morozov <morozov.e@edu.spbstu.ru>
2019-09-16 13:36:48 +0300

    Some resource changes.

--- app/src/main/java/com/morozov/psychology/ui/fragments/diary/DiaryFragment.kt
+++ app/src/main/java/com/morozov/psychology/ui/fragments/diary/DiaryFragment.kt
@@ -1,15 +1,5 @@
 package com.morozov.psychology.ui.fragments.diary
 
-import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.morozov.psychology.R
+class DiaryFragment {
 
-class DiaryFragment : Fragment() {
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.example_test_layout, container, false)
-    }
 }
