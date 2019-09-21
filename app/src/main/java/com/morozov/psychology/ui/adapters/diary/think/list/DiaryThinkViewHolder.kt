3c5b9bf4fd7c77302fcd00ac7b5f118177f2c2c3
Yevgeniy Morozov <morozov.e@edu.spbstu.ru>
2019-09-21 16:33:29 +0300

    Diary think editor layout added.

--- app/src/main/java/com/morozov/psychology/ui/adapters/diary/think/list/DiaryThinkViewHolder.kt
+++ app/src/main/java/com/morozov/psychology/ui/adapters/diary/think/list/DiaryThinkViewHolder.kt
@@ -7,7 +7,7 @@ import kotlinx.android.synthetic.main.item_diary_think_card.view.*
 class DiaryThinkViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
     fun populate(element: Pair<String, String>) {
-        itemView.textDiarySituation.text = element.first
-        itemView.textDiaryTime.text = element.second
+        itemView.textDiarySituation.text = element.second
+        itemView.textDiaryTime.text = element.first
     }
 }
