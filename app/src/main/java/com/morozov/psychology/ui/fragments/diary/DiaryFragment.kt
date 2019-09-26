7448fd102368da746ac7b551e3b959d141921dcb
Yevgeniy Morozov <morozov.e@edu.spbstu.ru>
2019-09-26 17:02:58 +0300

    Calendar max date set.

--- app/src/main/java/com/morozov/psychology/ui/fragments/diary/DiaryFragment.kt
+++ app/src/main/java/com/morozov/psychology/ui/fragments/diary/DiaryFragment.kt
@@ -135,11 +135,19 @@ class DiaryFragment:
     }
 
     override fun showCalendar() {
-        DatePickerDialog(context, calendarListener,
+        val dialog = DatePickerDialog(
+            context, calendarListener,
             calendar.get(Calendar.YEAR),
             calendar.get(Calendar.MONTH),
-            calendar.get(Calendar.DAY_OF_MONTH))
-            .show()
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+
+        val dayMtYrFormat = SimpleDateFormat("dd/MM/yyyy")
+        val dateCal = dayMtYrFormat
+            .parse("${calendar.get(Calendar.DAY_OF_MONTH)}/${calendar.get(Calendar.MONTH) + 1}/${calendar.get(Calendar.YEAR)}")
+
+        dialog.datePicker.maxDate = dateCal.time
+        dialog.show()
     }
 
     /*
