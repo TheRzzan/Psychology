40992efa16dea7d0182a3bebec50d6f6e91d3231
Yevgeniy Morozov <morozov.e@edu.spbstu.ru>
2019-10-01 18:24:49 +0300

    Tests: constants added.

--- app/src/main/java/com/morozov/psychology/utility/AppConstants.kt
+++ app/src/main/java/com/morozov/psychology/utility/AppConstants.kt
@@ -9,5 +9,14 @@ class AppConstants {
         // Diary section
         const val DIARY_IS_NEW_ITEM = "diary_is_new_item"
         const val DIARY_SELECTED_DAY ="diary_selected_day"
+
+        // Tests section
+        const val WEISMAN_BACK_TEST = "weisman_back_test"
+        const val ELLIS_TEST = "ellis_test"
+        const val HOSPITAL_SCALE_TEST = "hospital_scale_test"
+        const val INTEGRATIVE_TEST = "integrative_test"
+        const val LAZARUS_QUESTIONNAIRE_TEST = "lazarus_questionnaire_test"
+        const val STYLE_INDEX_TEST = "style_index_test"
+        const val SELF_ATTITUDE_TEST = "self_attitude_test"
     }
 }
