40fd4227c65f95ae5b5f7d9e69a244837b6d0cb5
Yevgeniy Morozov <morozov.e@edu.spbstu.ru>
2019-10-09 15:04:54 +0300

    Diary dates color days added.

--- app/src/main/java/com/morozov/psychology/ui/adapters/diary/date/DiaryDateViewHolder.kt
+++ app/src/main/java/com/morozov/psychology/ui/adapters/diary/date/DiaryDateViewHolder.kt
@@ -1,7 +1,12 @@
 package com.morozov.psychology.ui.adapters.diary.date
 
+import android.annotation.SuppressLint
+import android.content.res.ColorStateList
+import android.support.v4.content.ContextCompat
+import android.support.v4.widget.ImageViewCompat
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import com.morozov.psychology.R
 import com.morozov.psychology.utility.DateConverter
 import kotlinx.android.synthetic.main.item_diary_date_card.view.*
 import java.text.SimpleDateFormat
@@ -9,6 +14,7 @@ import java.util.*
 
 class DiaryDateViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
+    @SuppressLint("RestrictedApi")
     fun populate(element: Date, listener: View.OnClickListener) {
         val dayFormat = SimpleDateFormat("dd")
         val monthFormat = SimpleDateFormat("MM")
@@ -17,5 +23,15 @@ class DiaryDateViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         itemView.textDiaryMonth.text = DateConverter.getStringMonthSimple(monthFormat.format(element))
         itemView.textDiaryYear.text = yearFormat.format(element)
         itemView.setOnClickListener(listener)
+
+        when (element.day) {
+            0 -> itemView.imageDiaryCircle.setSupportBackgroundTintList(ContextCompat.getColorStateList(itemView.context, R.color.sunday))
+            1 -> itemView.imageDiaryCircle.setSupportBackgroundTintList(ContextCompat.getColorStateList(itemView.context, R.color.monday))
+            2 -> itemView.imageDiaryCircle.setSupportBackgroundTintList(ContextCompat.getColorStateList(itemView.context, R.color.tuesday))
+            3 -> itemView.imageDiaryCircle.setSupportBackgroundTintList(ContextCompat.getColorStateList(itemView.context, R.color.wednesday))
+            4 -> itemView.imageDiaryCircle.setSupportBackgroundTintList(ContextCompat.getColorStateList(itemView.context, R.color.thursday))
+            5 -> itemView.imageDiaryCircle.setSupportBackgroundTintList(ContextCompat.getColorStateList(itemView.context, R.color.friday))
+            6 -> itemView.imageDiaryCircle.setSupportBackgroundTintList(ContextCompat.getColorStateList(itemView.context, R.color.saturday))
+        }
     }
 }
