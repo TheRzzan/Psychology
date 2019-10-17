0b9f2fed61000ac3f76cb3fdc6a5fab5b0557667
Yevgeniy Morozov <morozov.e@edu.spbstu.ru>
2019-10-17 18:10:03 +0300

    Mind Change mc deprecation is ready checking added.

--- app/src/main/java/com/morozov/psychology/ui/fragments/mind/change/deprecation/MCDeprecation_2_Fragment.kt
+++ app/src/main/java/com/morozov/psychology/ui/fragments/mind/change/deprecation/MCDeprecation_2_Fragment.kt
@@ -1,9 +1,13 @@
 package com.morozov.psychology.ui.fragments.mind.change.deprecation
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.SeekBar
 import com.arellomobile.mvp.MvpAppCompatFragment
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.morozov.psychology.R
@@ -27,5 +31,69 @@ class MCDeprecation_2_Fragment: MvpAppCompatFragment(), MCDeprecation_2_View {
         buttonNext.setOnClickListener {
             mActivityPresenter.showMCDeprecation_3()
         }
+
+        seekBarDep1.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                textDepPercent1.text = "$progress%"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+
+        seekBarDep2.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                textDepPercent2.text = "$progress%"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+
+        addEditVerifyListnr(editDepAns)
+
+        verifyIsReadyToSave()
+    }
+
+    /*
+        * Helper functions
+        *
+        * */
+    private fun addEditVerifyListnr(editText: EditText) {
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                verifyIsReadyToSave()
+            }
+        })
+    }
+
+    private fun verifyIsReadyToSave() {
+        when(isReadyToSave()) {
+            true -> buttonNext.setBackgroundResource(R.drawable.rectangle_button)
+            false -> buttonNext.setBackgroundResource(R.drawable.rectangle_button_disable)
+        }
+        buttonNext.isEnabled = isReadyToSave()
+    }
+
+    private fun isReadyToSave(): Boolean {
+        return editDepAns.text.toString().isNotEmpty()
     }
 }
--- app/src/main/java/com/morozov/psychology/ui/fragments/mind/change/deprecation/MCDeprecation_3_Fragment.kt
+++ app/src/main/java/com/morozov/psychology/ui/fragments/mind/change/deprecation/MCDeprecation_3_Fragment.kt
@@ -1,9 +1,17 @@
 package com.morozov.psychology.ui.fragments.mind.change.deprecation
 
+import android.arch.lifecycle.MutableLiveData
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.SeekBar
+import android.widget.TextView
 import com.arellomobile.mvp.MvpAppCompatFragment
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.morozov.psychology.R
@@ -18,6 +26,8 @@ class MCDeprecation_3_Fragment: MvpAppCompatFragment(), MCDeprecation_3_View {
     lateinit var mPresenter: MCDeprecation_3_Presenter
     lateinit var mActivityPresenter: MainPresenter
 
+    private val selectedEmotion = MutableLiveData<Int>()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
         inflater.inflate(R.layout.mind_change_depreciation_3_layout, container, false)
 
@@ -26,6 +36,188 @@ class MCDeprecation_3_Fragment: MvpAppCompatFragment(), MCDeprecation_3_View {
 
         buttonAddNewThink.setOnClickListener {
             mActivityPresenter.showMindChangeSection()
+        }
+
+        addEditVerifyListnr(editDepAns1)
+        addEditVerifyListnr(editDepAns2)
+        addEditVerifyListnr(editDepAns3)
+
+        selectedEmotion.observeForever {
+            verifyIsReadyToSave()
+
+            if (it != null) {
+                setSelectedEmotion(it)
+                textDepEmotion.visibility = View.VISIBLE
+                textDepEmotionsPercent.visibility = View.VISIBLE
+                seekBarDepEmotions.visibility = View.VISIBLE
+                seekBarDepEmotions.progress = 1
+            } else {
+                setSelectedEmotion(-1)
+                textDepEmotion.visibility = View.GONE
+                textDepEmotionsPercent.visibility = View.GONE
+                seekBarDepEmotions.visibility = View.GONE
+            }
+        }
+        seekBarDepEmotions.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                textDepEmotionsPercent.text = "$progress%"
+
+                when (selectedEmotion.value) {
+                    0 -> textJoy.text = "$progress%"
+                    1 -> textSadness.text = "$progress%"
+                    2 -> textAnnoyance.text = "$progress%"
+                    3 -> textAnxiety.text = "$progress%"
+                    4 -> textDisgust.text = "$progress%"
+                    5 -> textInterest.text = "$progress%"
+                    6 -> textGuilt.text = "$progress%"
+                    7 -> textResentment.text = "$progress%"
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+
+        setEmotionsOnClick()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        selectedEmotion.value = null
+    }
+
+    /*
+        * Helper functions
+        *
+        * */
+    private fun addEditVerifyListnr(editText: EditText) {
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                verifyIsReadyToSave()
+            }
+        })
+    }
+
+    private fun verifyIsReadyToSave() {
+        when(isReadyToSave()) {
+            true -> buttonAddNewThink.setBackgroundResource(R.drawable.rectangle_button)
+            false -> buttonAddNewThink.setBackgroundResource(R.drawable.rectangle_button_disable)
+        }
+        buttonAddNewThink.isEnabled = isReadyToSave()
+    }
+
+    private fun isReadyToSave(): Boolean {
+        return  editDepAns1.text.toString().isNotEmpty()&&
+                editDepAns2.text.toString().isNotEmpty()&&
+                editDepAns3.text.toString().isNotEmpty() &&
+                selectedEmotion.value != null
+    }
+
+    private fun setEmotionsOnClick() {
+        imageJoy.setOnClickListener {
+            if (selectedEmotion.value == 0)
+                selectedEmotion.value = null
+            else
+                selectedEmotion.value = 0
+        }
+        imageSadness.setOnClickListener {
+            if (selectedEmotion.value == 1)
+                selectedEmotion.value = null
+            else
+                selectedEmotion.value = 1
+        }
+        imageAnnoyance.setOnClickListener {
+            if (selectedEmotion.value == 2)
+                selectedEmotion.value = null
+            else
+                selectedEmotion.value = 2
+        }
+        imageAnxiety.setOnClickListener {
+            if (selectedEmotion.value == 3)
+                selectedEmotion.value = null
+            else
+                selectedEmotion.value = 3
+        }
+        imageDisgust.setOnClickListener {
+            if (selectedEmotion.value == 4)
+                selectedEmotion.value = null
+            else
+                selectedEmotion.value = 4
+        }
+        imageInterest.setOnClickListener {
+            if (selectedEmotion.value == 5)
+                selectedEmotion.value = null
+            else
+                selectedEmotion.value = 5
+        }
+        imageGuilt.setOnClickListener {
+            if (selectedEmotion.value == 6)
+                selectedEmotion.value = null
+            else
+                selectedEmotion.value = 6
+        }
+        imageResentment.setOnClickListener {
+            if (selectedEmotion.value == 7)
+                selectedEmotion.value = null
+            else
+                selectedEmotion.value = 7
+        }
+    }
+
+    private fun setSelectedEmotion(num: Int) {
+        setEmotion(num == 0, getString(R.string.joy), resources.getDrawable(R.drawable.emotion_active_joy),
+            resources.getDrawable(R.drawable.emotion_non_active_joy), imageJoy, textJoy)
+
+        setEmotion(num == 1, getString(R.string.sadness), resources.getDrawable(R.drawable.emotion_active_sedness),
+            resources.getDrawable(R.drawable.emotion_non_active_sedness), imageSadness, textSadness)
+
+        setEmotion(num == 2, getString(R.string.annoyance), resources.getDrawable(R.drawable.emotion_active_annoyance),
+            resources.getDrawable(R.drawable.emotion_non_active_annoyance), imageAnnoyance, textAnnoyance)
+
+        setEmotion(num == 3, getString(R.string.anxiety), resources.getDrawable(R.drawable.emotion_active_anxiety),
+            resources.getDrawable(R.drawable.emotion_non_active_anxiety), imageAnxiety, textAnxiety)
+
+        setEmotion(num == 4, getString(R.string.disgust), resources.getDrawable(R.drawable.emotion_active_disgust),
+            resources.getDrawable(R.drawable.emotion_non_active_disgust), imageDisgust, textDisgust)
+
+        setEmotion(num == 5, getString(R.string.interest), resources.getDrawable(R.drawable.emotion_active_interest),
+            resources.getDrawable(R.drawable.emotion_non_active_interest), imageInterest, textInterest)
+
+        setEmotion(num == 6, getString(R.string.guilt), resources.getDrawable(R.drawable.emotion_active_guilt),
+            resources.getDrawable(R.drawable.emotion_non_active_guilt), imageGuilt, textGuilt)
+
+        setEmotion(num == 7, getString(R.string.resentment), resources.getDrawable(R.drawable.emotion_active_resentment),
+            resources.getDrawable(R.drawable.emotion_non_active_resentment), imageResentment, textResentment)
+    }
+
+    private fun setEmotion(b: Boolean, name: String, draw1: Drawable, draw2: Drawable,
+                           image: ImageView, text: TextView
+    ) {
+        when (b) {
+            true -> {
+                text.visibility = View.VISIBLE
+                textDepEmotion.text = name
+                text.text = "1%"
+                image.setImageDrawable(draw1)
+            }
+            false -> {
+                image.setImageDrawable(draw2)
+                text.visibility = View.GONE
+            }
         }
     }
 }
