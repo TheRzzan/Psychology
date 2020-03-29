9ba49f2bd34b0fec13e0bea9581d0d275042d0a8
e_morozov <e.morozov@social-solutions.ru>
2020-03-29 21:28:44 +0300

    Text and edit renderer added.

---
+++ app/src/main/java/com/morozov/psychology/ui/fragments/deep/mind/renderers/text/and/edit/OnTextUpdatedCallback.kt
@@ -0,0 +1,5 @@
+package com.morozov.psychology.ui.fragments.deep.mind.renderers.text.and.edit
+
+interface OnTextUpdatedCallback {
+    fun onUpdated(position: Int, newText: String)
+}
---
+++ app/src/main/java/com/morozov/psychology/ui/fragments/deep/mind/renderers/text/and/edit/TextAndEditModel.kt
@@ -0,0 +1,4 @@
+package com.morozov.psychology.ui.fragments.deep.mind.renderers.text.and.edit
+
+data class TextAndEditModel(val position: Int, val headerText: String,
+                            val hint: String, val insertedText: String? = null)
