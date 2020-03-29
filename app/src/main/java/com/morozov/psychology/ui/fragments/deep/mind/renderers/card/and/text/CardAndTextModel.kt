7351cead965062591b1bfbfd199ca8f77df6e6d8
e_morozov <e.morozov@social-solutions.ru>
2020-03-29 21:21:15 +0300

    Card and text renderer added.

---
+++ app/src/main/java/com/morozov/psychology/ui/fragments/deep/mind/renderers/card/and/text/CardAndTextModel.kt
@@ -0,0 +1,3 @@
+package com.morozov.psychology.ui.fragments.deep.mind.renderers.card.and.text
+
+data class CardAndTextModel(val position: Int, val text: String)
---
+++ app/src/main/java/com/morozov/psychology/ui/fragments/deep/mind/renderers/card/and/text/OnCardClickListener.kt
@@ -0,0 +1,5 @@
+package com.morozov.psychology.ui.fragments.deep.mind.renderers.card.and.text
+
+interface OnCardClickListener {
+    fun onClick(position: Int)
+}
