5174198b62d1bd6d7b31d25fa8ce88ba20d04eaf
e_morozov <e.morozov@social-solutions.ru>
2020-04-09 09:02:18 +0300

    fix-11 Percent in card model added.

--- app/src/main/java/com/morozov/psychology/ui/fragments/deep/mind/renderers/card/and/text/CardAndTextModel.kt
+++ app/src/main/java/com/morozov/psychology/ui/fragments/deep/mind/renderers/card/and/text/CardAndTextModel.kt
@@ -2,4 +2,4 @@ package com.morozov.psychology.ui.fragments.deep.mind.renderers.card.and.text
 
 import com.github.vivchar.rendererrecyclerviewadapter.ViewModel
 
-data class CardAndTextModel(val position: Int, val text: String): ViewModel
+data class CardAndTextModel(val position: Int, val text: String, val percent: Int): ViewModel
